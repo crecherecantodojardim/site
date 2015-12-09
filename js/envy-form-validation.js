f35139bc325a2ba95ed5066bd8656a95057ba043
Vagner Oliveira <vagner@vnda.com.br>
2015-12-09 19:00:55 -0200

    Atualização

---
+++ js/envy-form-validation.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+
+ $('#contact-form').validate({
+  rules: {
+    name: {
+      minlength: 2,
+      required: true
+    },
+    email: {
+      required: true,
+      email: true
+    },
+    message: {
+      minlength: 2,
+      required: true
+    }
+  },
+  highlight: function(element) {
+    $(element)
+    .closest('.control-group').removeClass('success').addClass('error');
+  },
+  submitHandler: function( form ) {
+      if( $("input[name=vnda]").val() == "" ){
+       form.submit();
+      }
+  },
+  success: function(element) {
+    element
+    .text('OK').addClass('valid')
+    .closest('.form-group').removeClass('error').addClass('success');
+  }
+ });
+ $("#mc-embedded-subscribe-form").validate({
+  highlight: function(element) {
+    // $(element)
+    // .closest('.control-group').removeClass('success').addClass('error');
+    $(element).removeClass('success').addClass('error');
+ 
+  },
+  submitHandler: function( form ) {
+      if( $("input[name=vnda]").val() == "" ){
+       form.submit();
+      }
+  },
+  success: function(element) {
+    $(element).removeClass('error').addClass('success');
+    // element
+    // .text('OK').addClass('valid')
+    // .closest('.form-group').removeClass('error').addClass('success');
+  }
+ });
+});
+
+$(document).ready(function(){
+
+ $('#blog-form').validate({
+  rules: {
+    name: {
+      minlength: 2,
+      required: true
+    },
+    email: {
+      required: true,
+      email: true
+    },
+    message: {
+      minlength: 2,
+      required: true
+    }
+  },
+  highlight: function(element) {
+    $(element)
+    .closest('.control-group').removeClass('success').addClass('error');
+  },
+  success: function(element) {
+    element
+    .text('OK').addClass('valid')
+    .closest('.form-group').removeClass('error').addClass('success');
+  }
+ });
+});
